a50b74cfbf310f6244fe4ca0da1367bdb71fa314
phanendra lanki <phanendralg12@gmail.com>
2023-12-09 12:16:14 +0530

    added package.json and package-lock.json

---
+++ data/users.js
@@ -0,0 +1,14 @@
+const users=[
+        {
+            id:"20A21A1237",
+            name:"phanendra",
+            email:"phanendralg12@gmail.com"
+        },
+        {
+            id:"20A312A",
+            name:"ganesh",
+            email:"Ganesh@gmail.com"
+        }
+    ]
+
+module.exports = users;
